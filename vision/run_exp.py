dfe7d3757fbd941d722f5175c69be9aeb8f95025
Ronak Mehta <rmehta004@gmail.com>
2020-07-30 16:46:40 +0000

    fix compute posteriors in vision.

--- vision/run_exp.py
+++ vision/run_exp.py
@@ -6,7 +6,7 @@ from utils import pull_data, load_data, fit_model, compute_posteriors
 # pull_data()
 
 train_x, train_y, test_x, test_y = load_data()
-# l2n = fit_model(train_x, train_y)
+l2n = fit_model(train_x, train_y)
 
 
 # Check accuracy on two tasks.
@@ -14,5 +14,5 @@ train_x, train_y, test_x, test_y = load_data()
 # Posteriors are of the form [n_test * 100],
 # where the first 10 columns is the psoterior
 # from task 1, next 10 for task 2, etc.
-# compute_posteriors(test_x, test_y, l2n)
+compute_posteriors(test_x, test_y, l2n)
 
