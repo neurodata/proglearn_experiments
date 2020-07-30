1318b297dcad8033a604695a8b9d83ec4856379f
Ubuntu <ronak@ronak2.zbo0qijau52ehdxabkadtyfmde.cx.internal.cloudapp.net>
2020-07-30 15:58:56 +0000

    Inspect data for all classes.

--- vision/run_exp.py
+++ vision/run_exp.py
@@ -4,10 +4,14 @@ from utils import pull_data, load_data, fit_model, compute_posteriors
 # Run experiment.
 
 # If first time, run pull_data to get the CIFAR 100 dataset.
-pull_data()
+# pull_data()
 
+import numpy as np
 train_x, train_y, test_x, test_y = load_data()
-l2n = fit_model(train_x, train_y)
+print(train_y[0][0:10])
+print(train_y[9][400:410])
+print(np.unique(train_y))
+# l2n = fit_model(train_x, train_y)
 
 
 # Check accuracy on two tasks.
