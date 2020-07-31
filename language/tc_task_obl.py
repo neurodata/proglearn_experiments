bcd28c1d245debc4347a8739e338a0fdf1317a66
Ronak Mehta <rmehta004@gmail.com>
2020-07-31 18:41:06 +0000

    Language task with 10x more data and 35 trees.

--- language/tc_task_obl.py
+++ language/tc_task_obl.py
@@ -2,11 +2,11 @@ from utils import get_source_tasks
 from task_obl_utils import fit_source_tasks, compute_posteriors
 
 # Experimental parameters.
-n_estimators = 10
+n_estimators = 35
 verbose = True
 
 # Source tasks.
-source_tasks = get_source_tasks()
+source_tasks = get_source_tasks(sub_yelp = 0.01, sub_imdb = 0.1, sub_amazon = 0.01)
 fit_source_tasks(source_tasks, n_estimators=n_estimators, verbose=verbose)
 compute_posteriors(source_tasks, n_estimators=n_estimators, verbose=verbose)
 
