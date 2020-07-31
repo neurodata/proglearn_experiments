950ef10dc7d50e4a1a51eac1e7034b7fcdb52511
Ubuntu <ronak@ronak3.zbo0qijau52ehdxabkadtyfmde.cx.internal.cloudapp.net>
2020-07-31 16:56:30 +0000

    CIFAR task prior experiment.

--- vision/run_task_priors.py
+++ vision/run_task_priors.py
@@ -1,5 +1,5 @@
 from utils import load_data, run_exp
 
 train_x, train_y, test_x, test_y = load_data(split=False, task_prior=True)
-run_exp(train_x, train_y, test_x, test_y, name="task_prior")
+run_exp(train_x, train_y, test_x, test_y, lr=0.003, epochs = 20, name="task_prior")
 
