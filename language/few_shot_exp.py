66d08c653751b141948446e42523c083b8a2e893
Ronak Mehta <t-romeht@microsoft.com>
2020-08-04 14:39:14 -0400

    Few shot experiment with single task learner baseline and average accuracy.

--- language/few_shot_exp.py
+++ language/few_shot_exp.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 
 from utils import get_source_and_target, load_toxic_comment
-from proglearn.forest import TransferForest
+from proglearn.forest import TransferForest, UncertaintyForest
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -18,22 +18,23 @@ target_name = "toxic_comment"
 source_tasks, target_task = get_source_and_target(source_names, target_name)
 
 # Train transformers on source data.
-tf = TransferForest(n_estimators=n_estimators)
-for task in source_tasks:
-    X_train, y_train, _, _ = task['load'](
-        verbose=verbose, 
-        subsample_frac=task['subsample_frac']
-    )
-    tf.add_source_task(X_train, y_train, task_id=task['id'])
+# tf = TransferForest(n_estimators=n_estimators)
+# for task in source_tasks:
+#     X_train, y_train, _, _ = task['load'](
+#         verbose=verbose, 
+#         subsample_frac=task['subsample_frac']
+#     )
+#     tf.add_source_task(X_train, y_train, task_id=task['id'])
 
-# Save the transfer forest with these source task.
-pickle.dump(tf, open("output/tf_source_trained_%d.p" % n_estimators, "wb"))
+# # Save the transfer forest with these source task.
+# pickle.dump(tf, open("output/tf_source_trained_%d.p" % n_estimators, "wb"))
 
 # Load target data, train voters, and compute accuracy.
 X_train_full, y_train_full, X_test, y_test = load_toxic_comment(verbose=verbose)
 
 np.random.seed(12345)
-accs = []
+tf_accs = []
+uf_accs = []
 for subsample_frac in subsample_fracs:
     _, X_train, _, y_train = train_test_split(
         X_train_full, 
@@ -45,10 +46,18 @@ for subsample_frac in subsample_fracs:
     tf = pickle.load(open("output/tf_source_trained_%d.p" % n_estimators, "rb"))
     tf.add_target_task(X_train, y_train, task_id=target_task['id'])
     y_pred = tf.predict(X_test)
-    accs.append(accuracy_score(y_pred, y_test))
+    acc = 1 - np.mean(np.abs(y_pred - y_test))
+    tf_accs.append(acc)
+
+    uf = UncertaintyForest(n_estimators=len(source_tasks) * n_estimators)
+    uf.fit(X_train, y_train)
+    y_pred = uf.predict(X_test)
+    acc = 1 - np.mean(np.abs(y_pred - y_test))
+    uf_accs.append(acc)
 
 pickle.dump(subsample_fracs, open("output/tf_subsample_fracs.p", "wb"))
-pickle.dump(accs, open("output/tf_accs.p", "wb"))
+pickle.dump(tf_accs, open("output/tf_accs.p", "wb"))
+pickle.dump(uf_accs, open("output/uf_accs.p", "wb"))
 
 
 
