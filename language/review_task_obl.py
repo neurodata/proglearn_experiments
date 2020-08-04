98962656bbe3970dae4917de0bdd45fb184341d9
Ronak Mehta <t-romeht@microsoft.com>
2020-08-04 15:43:30 -0400

    Zero shot comparisons.

--- language/review_task_obl.py
+++ language/review_task_obl.py
@@ -1,29 +1,32 @@
-from task_obl_utils import run_zero_shot
+from task_obl_utils import run_zero_shot, run_task_aware_pl, run_single_task
 
 # Hyperparameters
 n_estimators = 10
 verbose = True
 
 # Multitask 0:
-# multitask_id = 0
-# print("MULTITASK 0: IMDB, AMAZON ---> YELP")
-# source_names = ["imdb", "amazon"]
-# target_name = "yelp"
+multitask_id = 0
+print("MULTITASK 0: IMDB, AMAZON ---> YELP")
+source_names = ["imdb", "amazon"]
+target_name = "yelp"
 
-# run_zero_shot(source_names, target_name, multitask_id, n_estimators, verbose)
+run_zero_shot(source_names, target_name, 0, n_estimators, verbose)
+run_task_aware_pl(source_names, target_name, 10, n_estimators, verbose)
+run_single_task(target_name, 20, n_estimators, verbose)
 
-# Multitask 1:
-multitask_id = 1
-print("MULTITASK 1: YELP, AMAZON ---> IMDB")
+
+print("MULTITASK: YELP, AMAZON ---> IMDB")
 source_names = ["yelp", "amazon"]
 target_name = "imdb"
 
-run_zero_shot(source_names, target_name, multitask_id, n_estimators, verbose)
+run_zero_shot(source_names, target_name, 1, n_estimators, verbose)
+run_task_aware_pl(source_names, target_name, 11, n_estimators, verbose)
+run_single_task(target_name, 21, n_estimators, verbose)
 
-# Multitask 2:
-multitask_id = 2
-print("MULTITASK 2: YELP, IMDB ---> AMAZON")
+print("MULTITASK: YELP, IMDB ---> AMAZON")
 source_names = ["yelp", "imdb"]
 target_name = "amazon"
 
-run_zero_shot(source_names, target_name, multitask_id, n_estimators, verbose)
+run_zero_shot(source_names, target_name, 2, n_estimators, verbose)
+run_task_aware_pl(source_names, target_name, 12, n_estimators, verbose)
+run_single_task(target_name, 22, n_estimators, verbose)
