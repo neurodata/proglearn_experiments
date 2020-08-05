5a87e7ee0dd47e636f0de1ebf5a7959feff824f3
Ronak Mehta <t-romeht@microsoft.com>
2020-08-04 20:49:14 -0400

    few shot with precision recall analysis.

--- vision/run_cifar100.py
+++ vision/run_cifar100.py
@@ -1,5 +1,4 @@
-from utils import pull_data, load_data, run_exp
+from utils import load_data, run_exp
 
-# pull_data()
 train_x, train_y, test_x, test_y = load_data(split=False)
-run_exp(train_x, train_y, test_x, test_y, epochs = 50, lr = 0.001, name='cifar100')
+run_exp(train_x, train_y, test_x, test_y, epochs=50, lr=0.001, name="cifar100")
