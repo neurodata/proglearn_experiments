f881d419fe85a23c9ce285e737cd9367eddff4f2
Ronak Mehta <t-romeht@microsoft.com>
2020-08-04 23:08:13 -0400

    CIFAR 100 with LF experiment.

--- vision/run_cifar100.py
+++ vision/run_cifar100.py
@@ -1,4 +1,18 @@
-from utils import load_data, run_exp
+import pickle
+from utils import load_embedded_data
+from proglearn.forest import LifelongClassificationForest
+from sklearn.metrics import accuracy_score
 
-train_x, train_y, test_x, test_y = load_data(split=False)
-run_exp(train_x, train_y, test_x, test_y, epochs=50, lr=0.001, name="cifar100")
+n_estimators = 100
+
+X_train, y_train, X_test, y_test = load_embedded_data(split=False)
+
+lf = LifelongClassificationForest(n_estimators=n_estimators)
+lf.add_task(X_train, y_train, task_id=0)
+
+y_pred = lf.predict(X_test, 0)
+acc = accuracy_score(y_test, y_pred)
+
+print("Accuracy of ResNet50 + L2F on CIFAR 100:", acc)
+pickle.dump(acc, open("output/cifar100_lf_acc.p", "wb"))
+
