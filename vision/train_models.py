c4898e7bab99624d6f4b692cfdf74df2f004d923
Ronak Mehta <t-romeht@microsoft.com>
2020-08-10 14:19:31 -0400

    Train models and leave validation data.

--- vision/train_models.py
+++ vision/train_models.py
@@ -4,15 +4,19 @@ from proglearn.forest import UncertaintyForest, LifelongClassificationForest
 from utils import load_embedded_data
 
 n_estimators = 100
+n_train = 4500  # Leave the last 500 for validation.
 
 X_train, y_train, X_test, y_test = load_embedded_data(split_train=True)
 
 lf = LifelongClassificationForest(n_estimators=n_estimators)
 
 for t in range(10):
+    train_x = X_train[t][0:n_train]
+    train_y = y_train[t][0:n_train]
+
     uf = UncertaintyForest(n_estimators=n_estimators)
-    uf.fit(X_train[t], y_train[t])
-    lf.add_task(X_train[t], y_train[t], task_id=t)
+    uf.fit(train_x, train_y)
+    lf.add_task(train_x, train_y, task_id=t)
     pickle.dump(uf, open("output/uf_task_%d.p" % t, "wb"))
 
 pickle.dump(lf, open("output/lf_task_10.p", "wb"))
